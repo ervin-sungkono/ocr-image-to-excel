64d099e7a2fef3e5c72cd765d92ee36463354ab4
ervin-sungkono <ervinsung@gmail.com>
2023-08-02 15:55:13 +0700

    fix issue, data not updated

---
+++ components/JobDetailCard.js
@@ -0,0 +1,54 @@
+import format from "date-fns/format"
+import { downloadExcel } from "@/lib/helper"
+import { useState } from "react"
+
+import { BiRefresh } from "react-icons/bi"
+
+export default function JobDetailCard({public_id, state, created_at, providers}){
+    const [status, setStatus] = useState(state)
+    const [loading, setLoading] = useState(false)
+
+    const updateStatus = async() => {
+        setLoading(true)
+        fetch(`/api/ocr-table/detail?id=${public_id}`)
+            .then(res => res.json())
+            .then(({status}) => setStatus(status))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+    }
+
+    const handleFileDownload = async(provider) => {
+        const jobDetail = await fetch(`/api/ocr-table/detail?id=${public_id}`)
+            .then(res => res.json())
+            .catch(err => console.log(err))
+        
+        downloadExcel(jobDetail, provider)
+    }
+
+    return (
+        <div className="relative p-4 pr-11 shadow-md rounded-md border border-black/30 text-sm">
+            <button className="absolute right-2 top-2 border border-gray-800 text-gray-800 font-semibold p-2 rounded-md hover:bg-gray-800 hover:text-white transition-colors" onClick={updateStatus}>
+                <BiRefresh size={20}/>
+            </button>
+            {!loading ? (
+                <>
+                    <p className="font-medium">ID: {public_id}</p>
+                    <p className="font-medium">Created at: {format(new Date(created_at), 'dd-MMM-yyyy HH:MM:SS')}</p>
+                    <p className="mt-2">Status: {status}</p>
+                    {status === "finished" && <div className="flex flex-col gap-2">
+                        <p className="font-semibold">Download results</p>
+                        <hr/>
+                        <div className="flex flex-wrap gap-2">
+                            {providers.split(',').map(provider => (
+                                <button className="py-2 px-4 bg-gray-800 text-white font-semibold rounded-md" onClick={() => handleFileDownload(provider)}>
+                                    {provider}
+                                </button>
+                            ))}
+                        </div>
+                    </div>}
+                </> 
+            ) : <p className="text-lg font-medium">Loading...</p>}
+            
+        </div>
+    )
+}
--- components/ListData.js
+++ components/ListData.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react"
-import format from "date-fns/format"
-import { downloadExcel } from "@/lib/helper"
 
 import { BiRefresh } from "react-icons/bi"
+import JobDetailCard from "./JobDetailCard"
 
 export default function ListData(){
     const [jobs, setJobs] = useState()
@@ -13,45 +12,21 @@ export default function ListData(){
             .then(({jobs}) => setJobs(jobs))
     }, [jobs])
 
-    const handleFileDownload = async(id, provider) => {
-        const jobDetail = await fetch(`/api/ocr-table/detail?id=${id}`)
-            .then(res => res.json())
-            .catch(err => console.log(err))
-
-        console.log(jobDetail)
-        
-        downloadExcel(jobDetail, provider)
-    }
-
     return(
         <div className="w-full max-w-4xl flex-grow flex flex-col gap-2">
             <div className="flex justify-between items-end">
                 <h2 className="text-lg font-bold">View Results</h2>
-                <button className="border border-gray-800 text-gray-800 font-semibold p-2 rounded-md hover:bg-gray-800 hover:text-white transition-colors" onClick={jobs ? () => setJobs(null) : null}>
-                    <BiRefresh size={20}/>
+                <button className="border flex items-center gap-1 border-gray-800 text-gray-800 font-semibold p-2 rounded-md hover:bg-gray-800 hover:text-white transition-colors" onClick={jobs ? () => setJobs(null) : null}>
+                    <BiRefresh size={20}/> <p>Refresh All</p>
                 </button>
             </div>
             <hr/>
             {jobs ? 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-2">
                 {jobs.map(job => (
-                    <div className="p-4 shadow-md rounded-md border border-black/30 text-sm">
-                        <p className="font-medium">ID: {job.public_id}</p>
-                        <p className="font-medium">Created at: {format(new Date(job.created_at), 'dd-MMM-yyyy HH:MM:SS')}</p>
-                        <p className="mt-2">Status: {job.state}</p>
-                        {job.state === "finished" && <div className="flex flex-col gap-2">
-                            <p className="font-semibold">Download results</p>
-                            <hr/>
-                            <div className="flex flex-wrap gap-2">
-                                {job.providers.split(',').map(provider => (
-                                    <button className="py-2 px-4 bg-gray-800 text-white font-semibold rounded-md" onClick={() => handleFileDownload(job.public_id, provider)}>{provider}</button>
-                                ))}
-                            </div>
-                        </div>}
-                        
-                    </div>
+                    <JobDetailCard {...job} key={job.public_id}/>
                 ))}
-            </div> : <>Loading...</>}
+            </div> : <p className="font-medium">Loading...</p>}
         </div>
     )
 }
